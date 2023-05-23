94afd2f8ecb099c4f615b65abf911740a8093d63
shaua <shauangel2000@gmail.com>
2023-05-23 15:21:11 +0800

    (experiments) v.2 flow statistical analysis of the scores

--- flow2_0.py
+++ flow2_0.py
@@ -1,5 +1,7 @@
 import json
 import os
+import csv
+import statistics
 
 
 def get_filename():
@@ -14,6 +16,42 @@ def get_filename():
 
 
 if __name__ == "__main__":
-    titles, data = get_filename()
-    for t in titles:
-        print(t)
+    titles, files = get_filename()
+    for i in range(len(titles)):
+        print(titles[i])
+        with open("stack_data/" + files[i], 'r', encoding='utf-8') as f:
+            data = json.load(f)
+            f.close()
+        scores = []
+        for q in data:
+            temp = []
+            for ans in q['answers']:
+                record = {'question_id': q['question']['id'],
+                          'answer_id' : ans['id'],
+                          'origin' : ans['score']}
+                temp.append(ans['score'])
+                scores.append(record)
+
+            # statistical analysis
+            # 3 kinds of normalization methods
+            for r in scores:
+                try:
+                    r['min_max'] = ( r['origin'] - min(temp) ) / ( max(temp) - min(temp) )
+                    r['mean'] = ( r['origin'] - statistics.mean(temp) ) / ( max(temp) - min(temp) )
+                    r['z-score'] = ( r['origin'] - statistics.mean(temp) ) / statistics.stdev(temp)
+                    print(r)
+                except Exception:
+                    r['min_max'] = 0
+                    r['mean'] = 0
+                    r["z-score"] = 0
+                    continue
+
+            # save as csv file
+            attrs = ['question_id', 'answer_id', 'origin', 'min_max', 'mean', 'z-score']
+            with open('model/questions/'+str(i+1)+"/ratings.csv", 'w', encoding="utf-8") as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=attrs)
+                writer.writeheader()
+                writer.writerows(scores)
+                csvfile.close()
+
+
